d1b002a51563356bb4619e401dc93f746610fcea
efetekce <efete_kce@proton.me>
2023-11-22 01:55:38 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(InMemoryProductDal ınMemoryProductDal)
+        {
+        }
+
+        public List<Product> GetAll()
+        {
+            // is kodlari - business logic
+            return _productDal.GetAll();
+        }
+    }
+}
